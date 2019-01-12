83ad6a2752191c352de5484e8e69882ad3f06ce6
Luis Lira <luislira1029@gmail.com>
2019-01-12 01:27:53 -0600

    Proyecto Economia

---
+++ simplex/src/models/Enlace.java
@@ -0,0 +1,33 @@
+package models;
+
+/**
+ *
+ * @author LLira
+ */
+public class Enlace {
+    private Object[] puntos = null;
+    private int[] FuncionObjetivo = null;
+
+    public Enlace() {
+    }
+
+    public Object[] getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(Object[] puntos) {
+        this.puntos = puntos;
+    }
+
+    /**
+     * @return the FuncionObjetivo
+     */
+    public int[] getFuncionObjetivo() {
+        return FuncionObjetivo;
+    }
+
+    public void setFuncionObjetivo(int[] FuncionObjetivo) {
+        this.FuncionObjetivo = FuncionObjetivo;
+    }
+        
+} // End
